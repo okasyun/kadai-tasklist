61b7b763bea1470d8db97e2b6a44696f487f261e
shun.okamoto <shunshun1207shun@gmail.com>
2021-09-23 11:18:15 +0000

    update error

--- routes/web.php
+++ routes/web.php
@@ -14,22 +14,3 @@
 
 Route::get('/', 'TasksController@index');
 Route::resource('tasks', 'TasksController');
-
-/*
-// デフォルトのコメント部分は省略
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-// CRUD
-// メッセージの個別詳細ページ表示
-Route::get('tasks/{id}',  'TasksController@show');
-
-Route::get('tasks/create', 'TasksController@create')->name('tasks.create');
-
-Route::get('tasks/{id}/edit', 'MessagesController@edit')->name('tasks.edit');
-
-// index: showの補助ページ
-Route::get('tasks', 'TasksController@index')->name('tasks.index');
-/*
