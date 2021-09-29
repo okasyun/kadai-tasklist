f7a913b1ef597b64260e31435ab2e71b84043f3c
shun.okamoto <shunshun1207shun@gmail.com>
2021-09-29 23:41:08 +0000

    update tasklist

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -49,8 +49,15 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
-         // タスクを作成
+        // バリデーション
+        $request->validate([
+            'status' => 'required|max:10',   // 追加
+            'content' => 'required',
+            ]);
+            
+         // タスクをå作成
         $task = new Task;
+        $task->status = $request->status;    // 追加
         $task->content = $request->content;
         $task->save();
 
@@ -102,9 +109,17 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        
+          // バリデーション
+        $request->validate([
+            'status' => 'required|max:10',   // 追加
+            'content' => 'required'
+        ]);
+        
          // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
         // タスクを更新
+        $task->status = $request->status; // 追加
         $task->content = $request->content;
         $task->save();
         
